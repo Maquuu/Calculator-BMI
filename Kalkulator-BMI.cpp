a2f85fbef49df4128b6f64ec9e3df9e47e8c3614
Maquuu <146543908+Maquuu@users.noreply.github.com>
2023-10-12 11:56:27 +0200

    Add files via upload

---
+++ Kalkulator-BMI.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	float waga;
+	float wzrost;
+	cout << "Podaj swoja wage w kg\n";
+	cin >> waga;
+	cout << "Podaj swoj wzrost w metrach";
+	cin >> wzrost;
+	float BMI = waga / (wzrost*wzrost);
+
+	cout << BMI;
+
+	if (BMI < 16) {
+		cout << "jestes wyglodzony";
+	}
+	else if(BMI > 16 && BMI < 17) {
+		cout << " jestes wychudzony";
+	}
+	else if (BMI > 17 && BMI < 18.5) {
+		cout << " Masz niedowage";
+	}
+	else if (BMI > 18.5 && BMI < 25) {
+		cout << " Masz prawidlowe BMI";
+	}
+	else if (BMI > 25 && BMI < 30) {
+		cout << " Masz nadwage";
+	}
+	else if (BMI > 30 && BMI < 35) {
+		cout <<  " Masz otylosc I stopnia";
+	}
+	else if (BMI > 35 && BMI < 40) {
+		cout << " Masz otylosc II stopnia";
+	}
+	else if (BMI > 40) {
+		cout << " Masz otylosc I stopnia";
+	}
+}
